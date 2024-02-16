10c238764145c21cafbe38744a2d62d5d6ac0a1c
olisabelm <caroline.isabel30@gmail.com>
2024-02-16 10:41:37 +0700

    file perubahan

--- login.php
+++ login.php
@@ -1,27 +1,36 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<title>Halaman Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Login</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-	<form action="validasi.php" method="POST">
-		<h1>Selamat Datang!</h1>
-		<table>
-			<tr>
-				<td>Username</td>
-				<td><input type="text" name="username"></td>
-			</tr>
-			<tr>
-				<td>Password</td>
-				<td><input type="password" name="password"></td>
-			</tr>
-			<tr>
-				<td colspan="2">
-					<button type="submit">LOGIN</button>
-					<button type="reset">CLEAR</button>
-				</td>
-			</tr>
-		</table>
-	</form>
-</body>	
+    <div class="container">
+        <form action="validasi.php" method="POST" class="login-form">
+            <h1>Selamat Datang!</h1>
+            <table>
+                <tr>
+                    <td>Username</td>
+                    <td><input type="text" name="username" class="input-field"></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type="password" name="password" class="input-field"></td>
+                </tr>
+                <tr>
+                    <td colspan="2" class="button-container">
+                        <button type="submit" class="login-button">LOGIN</button>
+                        <button type="reset" class="clear-button">CLEAR</button>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</body>
+
 </html>
--- toko-caroline.sql
+++ toko-caroline.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 26 Jan 2024 pada 05.34
--- Versi server: 10.1.38-MariaDB
--- Versi PHP: 7.3.2
+-- Generation Time: Feb 16, 2024 at 04:32 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang`
+-- Table structure for table `barang`
 --
 
 CREATE TABLE `barang` (
@@ -35,25 +34,43 @@ CREATE TABLE `barang` (
   `stok` int(10) UNSIGNED NOT NULL,
   `harga_beli` int(10) UNSIGNED NOT NULL,
   `harga_jual` int(10) UNSIGNED NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `barang`
+-- Dumping data for table `barang`
 --
 
 INSERT INTO `barang` (`id`, `nama`, `kategori`, `stok`, `harga_beli`, `harga_jual`, `created_at`, `updated_at`) VALUES
-(1, 'chocolatos', 'makanan', 10, 8000, 10000, '2024-01-25 01:10:40', '2024-01-26 04:10:05'),
-(2, 'matcha', 'minuman', 15, 5000, 10000, '2024-01-25 01:10:40', '2024-01-26 03:54:45'),
-(3, 'choco', 'makanan', 5, 10000, 15000, '2024-01-25 01:10:40', '2024-01-26 03:54:39'),
-(4, 'oreo', 'minuman', 0, 10000, 15000, '2024-01-25 01:10:40', '2024-01-26 03:54:50'),
-(5, 'strawberry', 'minuman', 1, 20000, 25000, '2024-01-25 01:10:40', '2024-01-26 03:54:57');
+(1, 'black forest', 'makanan', 10, 8000, 10000, '2024-01-25 01:10:40', '2024-02-15 16:54:49'),
+(2, 'cappuccino', 'minuman', 10, 5000, 10000, '2024-01-25 01:10:40', '2024-02-15 16:55:04'),
+(3, 'chesse cakes', 'makanan', 5, 10000, 15000, '2024-01-25 01:10:40', '2024-02-15 16:55:16'),
+(4, 'irish coffe', 'minuman', 10, 10000, 15000, '2024-01-25 01:10:40', '2024-02-15 16:55:27'),
+(5, 'vancho', 'minuman', 5, 20000, 25000, '2024-01-25 01:10:40', '2024-02-15 16:55:36'),
+(8, 'red velvet', 'makanan', 10, 10000, 15000, '2024-02-15 17:04:48', NULL),
+(9, 'fruit cakes', 'makanan', 12, 12000, 20000, '2024-02-15 17:04:48', NULL),
+(10, 'matcha cakes', 'makanan', 10, 15000, 20000, '2024-02-15 17:04:48', NULL),
+(11, 'dalgona coffe', 'minuman', 20, 8000, 10000, '2024-02-15 17:04:48', NULL),
+(12, 'Gooday', 'minuman', 15, 5000, 10000, '2024-02-15 17:04:48', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pembelian`
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `nomor_telepon` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembelian`
 --
 
 CREATE TABLE `pembelian` (
@@ -62,12 +79,12 @@ CREATE TABLE `pembelian` (
   `jumlah` int(10) UNSIGNED NOT NULL,
   `total_harga` int(10) UNSIGNED NOT NULL,
   `id_staff` int(10) UNSIGNED NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `pembelian`
+-- Dumping data for table `pembelian`
 --
 
 INSERT INTO `pembelian` (`id`, `id_barang`, `jumlah`, `total_harga`, `id_staff`, `created_at`, `updated_at`) VALUES
@@ -75,12 +92,14 @@ INSERT INTO `pembelian` (`id`, `id_barang`, `jumlah`, `total_harga`, `id_staff`,
 (2, 2, 20, 100000, 2, '2024-01-25 01:20:04', NULL),
 (3, 4, 5, 50000, 5, '2024-01-25 01:20:04', NULL),
 (4, 5, 10, 200000, 4, '2024-01-25 01:20:04', '2024-01-25 01:36:15'),
-(5, 1, 30, 150000, 1, '2024-01-25 01:20:04', NULL);
+(6, 4, 10, 100000, 2, '2024-01-31 13:53:07', NULL),
+(7, 5, 4, 80000, 3, '2024-01-31 13:53:52', NULL),
+(8, 1, 2, 16000, 5, '2024-01-31 13:54:41', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `penjualan`
+-- Table structure for table `penjualan`
 --
 
 CREATE TABLE `penjualan` (
@@ -89,25 +108,26 @@ CREATE TABLE `penjualan` (
   `jumlah` int(10) UNSIGNED NOT NULL,
   `total_harga` int(10) UNSIGNED NOT NULL,
   `id_staff` int(10) UNSIGNED NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `penjualan`
+-- Dumping data for table `penjualan`
 --
 
 INSERT INTO `penjualan` (`id`, `id_barang`, `jumlah`, `total_harga`, `id_staff`, `created_at`, `updated_at`) VALUES
 (1, 3, 5, 75000, 1, '2024-01-25 01:23:03', '2024-01-26 03:57:26'),
 (2, 2, 5, 50000, 2, '2024-01-25 01:23:03', '2024-01-26 03:56:55'),
 (3, 4, 5, 75000, 5, '2024-01-25 01:23:03', '2024-01-26 03:58:13'),
-(4, 1, 5, 50000, 3, '2024-01-25 01:23:03', '2024-01-26 03:58:04'),
-(5, 5, 9, 225000, 4, '2024-01-25 01:23:03', '2024-01-26 03:58:54');
+(5, 5, 9, 225000, 4, '2024-01-25 01:23:03', '2024-01-26 03:58:54'),
+(6, 2, 5, 50000, 2, '2024-01-31 13:18:52', NULL),
+(7, 1, 2, 20000, 3, '2024-01-31 13:20:18', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -115,12 +135,12 @@ CREATE TABLE `user` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `level` enum('admin','keuangan','logistik') NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `level`, `created_at`, `updated_at`) VALUES
@@ -136,13 +156,19 @@ INSERT INTO `user` (`id`, `username`, `password`, `level`, `created_at`, `update
 --
 
 --
--- Indeks untuk tabel `barang`
+-- Indexes for table `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `pembelian`
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pembelian`
 --
 ALTER TABLE `pembelian`
   ADD PRIMARY KEY (`id`),
@@ -150,7 +176,7 @@ ALTER TABLE `pembelian`
   ADD KEY `id_staff` (`id_staff`);
 
 --
--- Indeks untuk tabel `penjualan`
+-- Indexes for table `penjualan`
 --
 ALTER TABLE `penjualan`
   ADD PRIMARY KEY (`id`),
@@ -158,52 +184,58 @@ ALTER TABLE `penjualan`
   ADD KEY `id_barang` (`id_barang`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `barang`
+-- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT untuk tabel `pembelian`
+-- AUTO_INCREMENT for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pembelian`
 --
 ALTER TABLE `pembelian`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT untuk tabel `penjualan`
+-- AUTO_INCREMENT for table `penjualan`
 --
 ALTER TABLE `penjualan`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `pembelian`
+-- Constraints for table `pembelian`
 --
 ALTER TABLE `pembelian`
   ADD CONSTRAINT `pembelian_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`),
   ADD CONSTRAINT `pembelian_ibfk_2` FOREIGN KEY (`id_staff`) REFERENCES `user` (`id`);
 
 --
--- Ketidakleluasaan untuk tabel `penjualan`
+-- Constraints for table `penjualan`
 --
 ALTER TABLE `penjualan`
   ADD CONSTRAINT `penjualan_ibfk_1` FOREIGN KEY (`id_staff`) REFERENCES `user` (`id`),
